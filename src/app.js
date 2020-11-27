b403805a2068dad5716ca253264b1e5646c6e754
Marques <marquesde2013@gmail.com>
2020-11-26 22:50:20 -0300

    styles css

--- src/app.js
+++ src/app.js
@@ -35,18 +35,18 @@ function app() {
               </div>
          
               <div className="container-agendamento">
-                <label className="col-md-4 control-label" htmlFor="type">Tipo de repetição</label>
-                <div className="col-md-4">
+                <label htmlFor="type">Tipo de repetição</label>
+                <div >
                   <div className="radio">
                     <label htmlFor="type-0">
                     <input type="radio" name="type" id="type-0" value="1" onChange />
-                        Somente ao Iniciar o Windows
+                    <span> Somente ao Iniciar o Windows </span>
                     </label>
 	                </div>
                   <div className="radio">
                     <label htmlFor="type-1">
                       <input type="radio" name="type" id="type-1" value="2"/>
-                        Repetir a cada
+                       <span> Repetir a cada </span>
                     </label>
                   </div>
                 </div>
@@ -54,7 +54,7 @@ function app() {
               <div className="container-comando">
                 <label  htmlFor="comando">Comando SQL</label>
                 <div >                     
-                  <textarea className="form-control" id="comando" name="comando" >Digite aqui o comando SQL</textarea>
+                  <textarea className="form-control" id="comando" name="comando" placeholder="Digite aqui o comando SQL"></textarea>
                 </div>
               </div>
 
@@ -72,26 +72,27 @@ function app() {
           </form>
           <div className="lista">
             <legend>Agendamentos</legend>
+              <div className="containter-filtro">
               <div className="form-group">
-                <label className="col-md-4 control-label" htmlFor="searchName">Texto do Comando</label>
+                <label htmlFor="searchName">Texto do Comando</label>
                 <div className="col-md-4">
                   <input id="searchinput" name="searchName" type="search" placeholder="Digite aqui a Pesquisa do Comando SQL" className="form-control input-md" />
                 </div>
               </div>
               <div className="form-group">
-                <label className="col-md-4 control-label" htmlFor="searchData">Data Source</label>
+                <label htmlFor="searchData">Data Source</label>
                 <div className="col-md-4">
                   <input id="searchinput" name="searchData" type="search" placeholder="Digite aqui " className="form-control input-md" />
                 </div>
               </div>
               <div className="form-group">
-                <label className="col-md-4 control-label" htmlFor="searchInitial">Initial Catalog</label>
+                <label  htmlFor="searchInitial">Initial Catalog</label>
                 <div className="col-md-4">
                   <input id="searchinput" name="searchInitial" type="search" placeholder="Digite aqui" className="form-control input-md" />
                 </div>
               </div>
               <div className="form-group">
-                <label className="col-md-4 control-label" htmlFor="searchStatus">Status</label>
+                <label  htmlFor="searchStatus">Status</label>
                 <div className="col-md-4">
                   <select id="searchStatus" name="searchStatus" className="form-control">
                     <option value="0">Desativo</option>
@@ -99,13 +100,14 @@ function app() {
                     </select>
                 </div>
               </div>
-              <div className="form-group">
-                <label className="col-md-4 control-label" htmlFor="submit"></label>
+              <div className="container-button-filtrar">
+                <label htmlFor="submit"></label>
                 <div className="col-md-4">
                   <button id="submit" name="submit" className="btn btn-primary" disabled>Filtrar...</button>
                 </div>
               </div>
-              </div>
+            </div>
+            </div>
        </div>
   );
 }
